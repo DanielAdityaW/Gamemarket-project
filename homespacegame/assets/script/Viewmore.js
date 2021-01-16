b4f9ff2bef5bbe72c0e8c30a9f09617532dadb9a
Venomeio <77531735+Venomeio@users.noreply.github.com>
2021-01-16 22:22:41 +0700

    Initial commit

---
+++ homespacegame/assets/script/Viewmore.js
@@ -0,0 +1,19 @@
+$(document).ready(function() 
+		{
+  			$("#toggle").click(function() 
+  			{
+    			var elem = $("#toggle").text();
+    			if (elem == "View More") 
+    			{
+      				//Stuff to do when btn is in the read more state
+      				$("#toggle").text("View Less");
+      				$("#text").slideDown();
+    			} 
+    			else 
+    			{
+      				//Stuff to do when btn is in the read less state
+      				$("#toggle").text("View More");
+      				$("#text").slideUp();
+    			}
+  			});
+		});
